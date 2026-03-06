ff8830cbcd47ceeee9a6fc298f112ddc3e9914df
soumyaranjansahu-learner <dm2soumya@gmail.com>
2026-03-06 14:46:35 +0530

    fix: Add missing views import to music urls

--- mine_music/music/urls.py
+++ mine_music/music/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import SongViewSet, PlaylistViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'songs', SongViewSet)
